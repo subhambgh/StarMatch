a424ceaf6eaae0d313d67013e6429af009e58e44
subhambgh <subhama.sit@gmail.com>
2020-02-10 04:39:57 -0500

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,124 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-export function App({ initialData }) {
-  const [count, setCount] = useState(0);
+import StarsDisplay from "./StarsDisplay";
+import utils from "../math-utils";
+// STAR MATCH - V5
+
+const PlayNumber = props => (
+  <button
+    className="number"
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => props.onClick(props.number, props.status)}
+  >
+    {props.number}
+  </button>
+);
+
+const PlayAgain = props => (
+  <div className="game-done">
+    <div
+      className="message"
+      style={{ color: props.gameStatus === "lost" ? "red" : "green" }}
+    >
+      {props.gameStatus === "lost" ? "Game Over" : "Nice"}
+    </div>
+    <button onClick={props.reset}>Play Again</button>
+  </div>
+);
+
+const Game = props => {
+  const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+  const [candidateNums, setCandidateNums] = useState([]);
+  const [secondsLeft, setSecondsLeft] = useState(10);
+  useEffect(() => {
+    if (secondsLeft > 0 && availableNums.length > 0) {
+      const timerId = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    }
+  });
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+  const gameStatus =
+    availableNums.length === 0 ? "won" : secondsLeft === 0 ? "lost" : "active";
+
+  const numberStatus = number => {
+    if (!availableNums.includes(number)) {
+      return "used";
+    }
+    if (candidateNums.includes(number)) {
+      return candidatesAreWrong ? "wrong" : "candidate";
+    }
+    return "available";
+  };
+
+  const onNumberClick = (number, currentStatus) => {
+    if (gameStatus !== "active" || currentStatus === "used") {
+      return;
+    }
+
+    const newCandidateNums =
+      currentStatus === "available"
+        ? candidateNums.concat(number)
+        : candidateNums.filter(cn => cn !== number);
+
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+  };
+
   return (
-    <div>
-      <h1>{initialData.appName}</h1>
-      This is a sample stateful and server-side rendered React application.
-      <br />
-      <br />
-      Here is a button that will track how many times you click it:
-      <br />
-      <br />
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          {gameStatus !== "active" ? (
+            <PlayAgain reset={props.startNewGame} gameStatus={gameStatus} />
+          ) : (
+            <StarsDisplay count={stars} />
+          )}
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map(number => (
+            <PlayNumber
+              key={number}
+              status={numberStatus(number)}
+              number={number}
+              onClick={onNumberClick}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: {secondsLeft}</div>
     </div>
   );
+};
+
+const StarMatch = () => {
+  const [gameId, setGameId] = useState(1);
+  return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />;
+};
+
+// Color Theme
+const colors = {
+  available: "lightgray",
+  used: "lightgreen",
+  wrong: "lightcoral",
+  candidate: "deepskyblue"
+};
+
+// Math science
+
+export function App() {
+  return <StarMatch />;
 }
